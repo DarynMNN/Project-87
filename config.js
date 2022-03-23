d91c491c1e8cab1602771a3435e97915f4288ea8
DarynMNN <87840884+DarynMNN@users.noreply.github.com>
2022-03-22 18:16:00 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCwZwY1jYFoFcm7L5sEaRd2oWIq1RUNYiI",
+    authDomain: "story-telling-app-5a264.firebaseapp.com",
+    projectId: "story-telling-app-5a264",
+    storageBucket: "story-telling-app-5a264.appspot.com",
+    messagingSenderId: "527691264760",
+    appId: "1:527691264760:web:e44e046f50192399f9010d"
+  };
